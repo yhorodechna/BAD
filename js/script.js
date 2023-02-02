6005f9175539c03b6fabe0528c57b52d6da8af0b
Yulia Horodechna <horodechnajulia@gmail.com>
2023-02-02 14:08:30 +0200

    some changes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const RESULT_TEXT_EL = document.getElementById("result__text")
 function showUniqueSymbolOfTextarea() {
     const textareaValue = document.getElementById("form__textarea").value;
     const uniqueSymbol = getUniqueSymbolFromSentence(textareaValue);
-    RESULT_TEXT_EL.innerHTML = uniqueSymbol ?
-        uniqueSymbol :
-        "Unique symbol not found!"
+    RESULT_TEXT_EL.innerHTML = uniqueSymbol 
+    ? uniqueSymbol 
+    : "Unique symbol not found!"
 }
